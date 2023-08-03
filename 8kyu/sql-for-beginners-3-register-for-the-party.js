be07ce8073050e7d02e2152c08629e52f6230ccf
Maxim Lagutin <max.lagutin.tmb@gmail.com>
2023-08-03 17:31:47 -0300

    add exercise

---
+++ 8kyu/sql-for-beginners-3-register-for-the-party.js
@@ -0,0 +1,18 @@
+// ❓ DESCRIPTION:
+// You received an invitation to an amazing party. Now you need to write an insert statement to add yourself to the table of participants.
+//
+//     participants table schema
+//
+// name (string)
+// age (integer)
+// attending (boolean)
+// NOTES:
+//
+//     Since alcohol will be served, you can only attend if you are 21 or older
+// You can't attend if the attending column returns anything but true
+
+
+// ✅ SOLUTION:
+// INSERT INTO participants VALUES ('Max', '24', true);
+//
+// SELECT * FROM participants;;
