15b1ba3304f54ce3f8a126ccdcca61ee60e28163
Maxim Lagutin <max.lagutin.tmb@gmail.com>
2023-08-04 13:42:58 -0300

    add exercise

---
+++ 7kyu/sql-basics-simple-group-by.js
@@ -0,0 +1,13 @@
+// ❓ DESCRIPTION:
+// For this challenge you need to create a simple GROUP BY statement, you want to group all the people by their age and count the people who have the same age.
+//
+//     people table schema
+// id
+// name
+// age
+// select table schema
+// age [group by]
+// people_count (people count)
+
+// ✅ SOLUTION:
+// SELECT COUNT (id) people_count, age FROM people GROUP BY age;
